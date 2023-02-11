f2a2f03d591667dec074e81f5cbd9794ca958250
Sean Maxwell <sa.maxwell@yahoo.com>
2023-02-11 09:40:29 -0600

    Initial commit

---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -0,0 +1,11 @@
+package com.polarbookshop.catalogservice.domain;
+
+import javax.xml.validation.Validator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookValidationTests {
+
+    private static Validator validator;
+
+}
